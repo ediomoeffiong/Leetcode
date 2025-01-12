8e98972e6cf29017def3156c2f0b24d4be9f634d
Covenant <ediomoemmaeffiong@gmail.com>
2025-01-12 21:01:40 +0100

    Added Longest Common Prefix problem Not yet done

---
+++ src/LongestCommonPrefix.java
@@ -0,0 +1,16 @@
+public class LongestCommonPrefix {
+}
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String strss = "";
+        for (int i = 0; i < strs.length; i++) {
+            for (int j = 0; j < strs[i].length(); j++) {
+                while (i > 0) {
+                    if (strs[i].charAt(j) == strs[i+1].charAt(j)) {
+                        strss = String.valueOf(strs[i].charAt(j));
+                    }
+                }
+                return strss;
+            }
+        }
